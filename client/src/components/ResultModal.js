87838655e875b7a99096eaa7ec1917acd7286674
freecree <bodya20126@gmail.com>
2023-08-09 15:59:36 +0300

    feat: add ukrainian and english languages
    - initialize i18next
    - add multi languages for Main, editor pages, modals and buttons

--- client/src/components/ResultModal.js
+++ client/src/components/ResultModal.js
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import {observer} from 'mobx-react-lite';
 import filesStore from '../store/filesStore.js';
+import { useTranslation } from 'react-i18next';
 
 import crossIcon from '../assets/icon-cross.svg';
 
 const ResultModal = ({active, closeModal}) => {
+    const {t, i18n} = useTranslation();
     const images = filesStore.files;
 
     const resultMarkings = images ?
@@ -22,7 +24,7 @@ const ResultModal = ({active, closeModal}) => {
             <div className='modal-content modal__result-content'
             onClick={e=>e.stopPropagation()}>
                 <img className='modal__cross-img' src={crossIcon} alt="Close" onClick={closeModal}/>
-                <h1 className='modal__title'>Розмітка жестів рук у форматі JSON</h1>
+                <h1 className='modal__title'>{t('modal.result-markup-title')}</h1>
                 <p className='modal__result-txt'>
                   {resultMarkings}
                 </p>
--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,34 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import i18next from 'i18next';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
+import translation_ua from './translations/ua/translation.json';
+import translation_en from './translations/en/translation.json';
+
+i18next
+    .use(initReactI18next)
+    .init({
+        debug: true,
+        lng: 'ua',
+        interpolation: {
+            escapeValue: false
+        },
+        resources: {
+            ua: {
+                translation: translation_ua
+            },
+            en: {
+                translation: translation_en
+            }
+        }
+    });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App/>
+    // <I18nextProvider i18n={i18next}>
+        <App/>
+    // </I18nextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
